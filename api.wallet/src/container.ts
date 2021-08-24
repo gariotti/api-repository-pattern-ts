d86aaa61a78a73a5b6ab84b5e6ab62fb90c2bc1d
Gus Ariotti <gustavo.ariotti@naranja.com>
2021-08-24 19:38:54 -0300

    repository pattern mysql impl

--- api.wallet/src/container.ts
+++ api.wallet/src/container.ts
@@ -2,6 +2,8 @@ import express =require ('express');
 import { asClass, createContainer } from 'awilix';
 import {TestService} from './services/test.service';
 import { scopePerRequest } from 'awilix-express';
+import { SubscriptionMySQLRepository } from './services/repositories/impl/mysql/subscription.repository';
+import { SubscriptionService } from './services/subscription.service';
 
 
 
@@ -9,7 +11,12 @@ export default ( app: express.Application) => {
     const container=createContainer({injectionMode: 'CLASSIC'});
 
     container.register({ 
-        testService: asClass(TestService).scoped()
+        //repositories
+        subscriptionRepository: asClass(SubscriptionMySQLRepository).scoped(),
+
+        //services
+        testService: asClass(TestService).scoped(),
+        subscriptionService: asClass(SubscriptionService).scoped()
     });
 
     app.use(scopePerRequest(container));
