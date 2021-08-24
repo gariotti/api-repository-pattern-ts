ee2fbb89678c946b5d17fd32538d045de28eb97e
Gus Ariotti <gustavo.ariotti@naranja.com>
2021-08-23 23:03:39 -0300

    first commit

---
+++ api.wallet/src/container.ts
@@ -0,0 +1,17 @@
+import express =require ('express');
+import { asClass, createContainer } from 'awilix';
+import {TestService} from './services/test.service';
+import { scopePerRequest } from 'awilix-express';
+
+
+
+export default ( app: express.Application) => {
+    const container=createContainer({injectionMode: 'CLASSIC'});
+
+    container.register({ 
+        testService: asClass(TestService).scoped()
+    });
+
+    app.use(scopePerRequest(container));
+
+};
